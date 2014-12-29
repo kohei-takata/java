91808726b10c375c536eb18bc9bfa25e2b79f140
tako-black <gennarishock+tako@gmail.com>
2014-12-29 10:03:05 +0900

    1-2-2

--- 1-2/Fibonacci.java
+++ 1-2/Fibonacci.java
@@ -2,6 +2,7 @@ class Fibonacci {
 	public static void main(String[] args){
 		int lo = 1;
 		int hi = 1;
+		System.out.println("フィボナッチ数列");
 		System.out.println(lo);
 		while(hi < 50){
 			System.out.println(hi);
