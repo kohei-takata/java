5290a50330927410fc2474f483de23c0f3f729ab
tako-black <gennarishock+tako@gmail.com>
2014-12-31 14:15:08 +0900

    1.4

--- 1-1/HelloWorld.java
+++ 1-1/HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld{
+	static final String HELLO = "Hello, World!";
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		System.out.println(HELLO);
 	}
 }
--- 1-2/Fibonacci.java
+++ 1-2/Fibonacci.java
@@ -1,10 +1,12 @@
 class Fibonacci {
+	static final String TITLE = "フィボナッチ数列";
+	static final int MAX = 50;
 	public static void main(String[] args){
 		int lo = 1;
 		int hi = 1;
-		System.out.println("フィボナッチ数列");
+		System.out.println(TITLE);
 		System.out.println(lo);
-		while(hi < 50){
+		while(hi < MAX){
 			System.out.println(hi);
 			hi = hi + lo;
 			lo = hi - lo;
