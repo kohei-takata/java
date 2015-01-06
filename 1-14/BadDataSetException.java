13ef552d1aeb9087da048d646e5c53bf698e308f
tako-black <gennarishock+tako@gmail.com>
2015-01-06 21:04:32 +0900

    1-14

---
+++ 1-14/BadDataSetException.java
@@ -0,0 +1,1 @@
+class BadDataSetException extends Exception {}
---
+++ 1-14/MyUtilities.java
@@ -0,0 +1,27 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+class MyUtilities {
+	public double [] getDataSet(String setName) throws BadDataSetException {
+		String file = setName + ".dset";
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(file);
+			return readDataSet(in);
+		} catch (IOException e) {
+			throw new BadDataSetException();
+		} finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException e) {
+				;
+			}
+		}
+	}
+	public double [] readDataSet(FileInputStream in) {
+		return new double[0];
+	}
+}
