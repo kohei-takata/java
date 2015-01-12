f91f519af0a9d49e657697ae342246e07aea76e3
tako-black <gennarishock+tako@gmail.com>
2015-01-12 23:39:52 +0900

    renshu2.5

--- 2-1/Vehicle.java
+++ 2-1/Vehicle.java
@@ -5,4 +5,14 @@ class Vehicle {
 	
 	public static long nextID;
 	public String number;
+
+	public static void main(String[] args) {
+		Vehicle vehicle1 = new Vehicle();
+		vehicle1.speed = 1;
+		vehicle1.houkou = "right";
+		vehicle1.nameOfOwner = "ore";
+		vehicle1.nextID = 02;
+		vehicle1.number = "01";
+		System.out.println(vehicle1.speed + "," + vehicle1.houkou + "," + vehicle1.nameOfOwner + "," + vehicle1.nextID + "," + vehicle1.number);
+	}
 }
