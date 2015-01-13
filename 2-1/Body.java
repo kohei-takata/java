baa3d37dfed9ecccebeaeccbc8e4a21edb5bb0ac
tako-black <gennarishock+tako@gmail.com>
2015-01-13 22:01:55 +0900

    renshu2.7

--- 2-1/Body.java
+++ 2-1/Body.java
@@ -1,7 +1,11 @@
 class Body {
 	public long idNum;
-	public String name;
-	public Body orbits;
+	public String name = "<unnamed>";
+	public Body orbits = null;
 
 	public static long nextID = 0;
+
+	Body() {
+		idNum = nextID++;
+	}
 }
--- 2-1/Vehicle.java
+++ 2-1/Vehicle.java
@@ -5,6 +5,14 @@ class Vehicle {
 	
 	public static long nextID;
 	public String number;
+	
+	public Vehicle() {
+	
+	}
+	
+	public Vehicle(String owner) {
+		nameOfOwner = owner;
+	}
 
 	public static void main(String[] args) {
 		Vehicle vehicle1 = new Vehicle();
@@ -14,5 +22,14 @@ class Vehicle {
 		vehicle1.nextID = 02;
 		vehicle1.number = "01";
 		System.out.println(vehicle1.speed + "," + vehicle1.houkou + "," + vehicle1.nameOfOwner + "," + vehicle1.nextID + "," + vehicle1.number);
+	
+	
+		Vehicle vehicle2 = new Vehicle("aitsu");
+		vehicle2.speed = 2;
+		vehicle2.houkou = "up";
+		vehicle2.nextID = 03;
+		vehicle2.number = "02";
+		System.out.println(vehicle2.speed + "," + vehicle2.houkou + "," + vehicle2.nameOfOwner + "," + vehicle2.nextID + "," + vehicle2.number);
+	
 	}
 }
