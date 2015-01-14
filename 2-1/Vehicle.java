7bf0664dd32d9c96201b16033528a4e077f6e737
tako-black <gennarishock+tako@gmail.com>
2015-01-14 23:46:29 +0900

    renshu2.9

--- 2-1/Vehicle.java
+++ 2-1/Vehicle.java
@@ -7,10 +7,11 @@ class Vehicle {
 	public String number;
 	
 	public Vehicle() {
-	
+		nextID++;	
 	}
 	
 	public Vehicle(String owner) {
+		this();
 		nameOfOwner = owner;
 	}
 
@@ -19,17 +20,21 @@ class Vehicle {
 		vehicle1.speed = 1;
 		vehicle1.houkou = "right";
 		vehicle1.nameOfOwner = "ore";
-		vehicle1.nextID = 02;
 		vehicle1.number = "01";
+		System.out.println(max());
 		System.out.println(vehicle1.speed + "," + vehicle1.houkou + "," + vehicle1.nameOfOwner + "," + vehicle1.nextID + "," + vehicle1.number);
 	
 	
 		Vehicle vehicle2 = new Vehicle("aitsu");
 		vehicle2.speed = 2;
 		vehicle2.houkou = "up";
-		vehicle2.nextID = 03;
 		vehicle2.number = "02";
+		System.out.println(max());
 		System.out.println(vehicle2.speed + "," + vehicle2.houkou + "," + vehicle2.nameOfOwner + "," + vehicle2.nextID + "," + vehicle2.number);
 	
+	}
+
+	public static long max() {
+		return nextID;
 	}
 }
